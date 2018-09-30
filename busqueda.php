3ba0818f7b8d58618c95860716e968d749834ca9
Jonathan <jonathancruz.e@gmail.com>
2018-09-30 16:18:56 -0300

    Busqueda General
    Pantalla de Busqueda General

---
+++ busqueda.php
@@ -0,0 +1,37 @@
+<?php
+//require_once('index.html');
+header('Content-type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");   // Permite acceso desde otros origenes, Solo para pruebas
+
+
+$post = json_decode(file_get_contents('php://input'), true);
+
+
+if(isset($post['palabra'])){
+    $resultado =  MLTraerProducto($post['palabra']);
+    echo json_encode($resultado);
+}
+
+
+function MLTraerProducto($art){
+   // echo "entre a la funcion";
+   // echo $art;
+    $url = "https://api.mercadolibre.com/sites/MLA/search?q=".$art;
+    $Productos = json_decode(file_get_contents($url),true);
+
+    $MLResult = array();
+    for ($i=0; $i <10 ; $i++) { 
+        # code...
+        $var = array();
+        $var[] = $Productos["results"][$i]["title"];
+        $var[] = $Productos["results"][$i]["permalink"];
+        $var[] = $Productos["results"][$i]["thumbnail"];
+        $var[] = $Productos["results"][$i]["price"]." ".$Productos["results"][$i]["currency_id"];
+
+        $MLResult[] = $var;
+    }
+
+    return($MLResult);
+}
+
+?>
