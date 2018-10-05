4fd8355ea41b9b891f9ab66b4a6724080d23dcd5
mariadriazola1986 <43587602+Mariadriazola1986@users.noreply.github.com>
2018-10-04 22:10:50 -0300

    se agrego alguna validacion

--- js/funciones.js
+++ js/funciones.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
 	$("#login").click(function() {
 		$("#formregistro").attr('id','formlogin');
 		$("#exampleModalLabel").text("Logueate");
+		$("#estado").val("Iniciar Sesion");
 		$("#ocultar").hide();
 	});
 	$("#registro").click(function() {
 		$("#formlogin").attr('id','formregistro');
 		$("#exampleModalLabel").text("Registrate");
 		$("#ocultar").show();
+		$("#estado").val("Registro");
 	});
 
 	$("#password,#passwordR").on('input', function(e) {
@@ -15,5 +17,48 @@ $(document).ready(function() {
         this.value = this.value.replace(/[^ a-z0-9áéíóúüñ]+/ig,"");
 	};
 	});
+
+	$("#passwordR").on("input",function(e){ 
+
+		if ($("#password").val()===$("#passwordR").val()){
+			$("#passwordR").attr('class', 'ok');
+			$("#password").attr('class', 'ok');
+		}
+
+		else
+		{
+			$("#passwordR").attr('class', 'error');
+			$("#password").attr('class', 'error');
+			
+		}
+		});
+	$("#password").on("input",function(e){ 
+
+		if ($("#password").val()===$("#passwordR").val()){
+			$("#passwordR").attr('class', 'ok');
+			$("#password").attr('class', 'ok');
+		}
+
+		else
+		{
+			$("#passwordR").attr('class', 'error');
+			$("#password").attr('class', 'error');
+			
+		}
+		});
+
+
+	
+	$("#estado").click(function() {
+		if ($("#password").val()===$("#passwordR").val()){
+			console.log("bien");
+		}
+		else{
+			console.log("mal");
+		}
+	});
+
+	
+
 
 });
