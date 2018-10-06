513160eca41d3dc24aef675e7d49c556f453338a
dhernanromero <dhernan.romero@gmail.com>
2018-10-06 18:11:01 -0300

    Modificacion
    Se modifica metodos dentro del archivo de prueba de web scraping

--- prueba_web_scraping/prueba_web_scraping.php
+++ prueba_web_scraping/prueba_web_scraping.php
@@ -6,9 +6,11 @@
         <a href="http://simplehtmldom.sourceforge.net/">Sitio de la libreria</a>
         <p>se debería poder indexar a esta pagina las imagenes y links de google pero tira error....</P>
         <?php
+            include('simple_html_dom.php');
             // Create DOM from URL or file
-            $html = file_get_html('http://www.google.com/');
+            $html = str_get_html('https://www.google.com.ar/');
 
+            print_r($html);
             // Find all images 
             foreach($html->find('img') as $element) 
                 echo $element->src . '<br>';
