9d14806951aae3dc94afcb9ac32f77ed3a46271f
alejarrua <alejarrua.stcc@gmail.com>
2018-10-18 06:28:10 -0300

    web scapping pagina fravega

---
+++ prueba_web_scraping/web_scraping_fravega/prototipo_busqueda_fravega.php
@@ -0,0 +1,44 @@
+<?php
+    require_once 'simple_html_dom.php';
+
+    function obtenerProductosFravega($productoBuscado)
+    {
+        $parseProductoBuscado = str_replace(' ', '%20', $productoBuscado);
+        $url = 'https://www.fravega.com/' . $parseProductoBuscado;
+        $listaProductos = array();
+
+        $html = file_get_html($url);
+
+        $container = $html->find('div[class=shelf-resultado n3colunas]', 0);
+        $ulList = $container->find('ul');
+
+        foreach($ulList as $ulItem)
+        {
+
+            $liList = $ulItem->find('li');
+
+            foreach($liList as $item)
+            {
+                $divWrap = $item->find('div[class=wrapData]', 0);
+                if(isset($divWrap))
+                {
+                    $h2 = $divWrap->find('h2', 0);
+                    $a = $h2->find('a', 0);
+                    $nombre = $a->innertext;
+                    $precio = $item->find('div[class=wrapData]', 0)->find('span[class=prodPrice]', 0)->find('em[class=BestPrice]', 0)->innertext;
+                    $imagen = $item->find('div[class=image]', 0)->find('a', 0)->find('img', 0)->src;
+                    $link = $item->find('div[class=wrapData]', 0)->find('h2', 0)->find('a', 0)->href;
+        
+                    $producto = new Producto;
+                    $producto->nombre = $nombre;
+                    $producto->precio = $precio;
+                    $producto->link = $link;
+                    $producto->urlImagen = $imagen;
+        
+                    $listaProductos[] = $producto;
+                }
+            }
+        }
+        return $listaProductos;
+    }
+?>
